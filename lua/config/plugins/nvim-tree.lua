fe2620dcd2480b30dfb0136a7261c2c608fe77e1
Oleksii Sukhodolskyi <o.sukhodolskyi@opentalk.eu>
2026-05-11 19:37:42 +0200

    feat: open nvim tree in cwd

--- lua/config/plugins/nvim-tree.lua
+++ lua/config/plugins/nvim-tree.lua
@@ -6,7 +6,10 @@ return {
 		"nvim-tree/nvim-web-devicons",
 	},
 	config = function()
-		require("nvim-tree").setup {}
+		require("nvim-tree").setup { update_focused_file = {
+			enable = true,
+			update_cwd = true,
+		}, }
 		vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<cr>", { desc = "Toggle file tree" })
 	end,
 }
